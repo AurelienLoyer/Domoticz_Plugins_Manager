60e01d14ea046962d741b3402c026613bad50f6c
Loyer Aurélien <aurelien.loyer@zenika.com>
2016-12-30 19:20:54 +0100

    new plugins save domoticz folder zip and move to ...

---
+++ save-domoticz-folder-php/index.php
@@ -0,0 +1,8 @@
+<?php
+
+//get config 
+// /path/to/save/destination_folder.zip
+// domoticz_save_2016-12-30.zip
+// /path/to/folder -> usb device ?
+
+exec('zip -r  /path/to/save/domoticz_save_2016-12-30.zip /path/to/folder');
