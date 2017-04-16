90c11749979dfbde4dd9899fa956284f996a8b25
Loyer Aurélien <aurelien.loyer@zenika.com>
2017-04-16 17:16:34 +0200

    Start config for save-domoticz-folder-php

--- save-domoticz-folder-php/index.php
+++ save-domoticz-folder-php/index.php
@@ -1,8 +1,32 @@
 <?php
 
-//get config 
+/**
+ * Save Domoticz configurator
+ * @author Aurélien Loyer
+ * @site aurelien-loyer.fr
+ * @github github.com/T3kstiil3
+ */
+
+
+// Dev mode ON :)
+$mod_debug = true;
+if($mod_debug){
+	error_reporting(E_ALL);  // On affiche les erreurs php
+	ini_set("display_errors", 1); // On affiche les erreurs php
+}
+
+$file_conf = 'config.json'; //fichier pour la conf
+
+$token = json_decode(file_get_contents($file_token,FILE_USE_INCLUDE_PATH),true);
+$conf = json_decode(file_get_contents($file_conf,FILE_USE_INCLUDE_PATH),true);
+
+//get config
 // /path/to/save/destination_folder.zip
 // domoticz_save_2016-12-30.zip
 // /path/to/folder -> usb device ?
 
-exec('zip -r  /path/to/save/domoticz_save_2016-12-30.zip /path/to/folder');
+$zip_name = 'domoticz_save_2016-12-30.zip';
+$save_destination = '/path/to/save/';
+$folder_2_save_path = '/path/to/domoticz';
+
+exec('zip -r  '.$save_destination.$zip_name' '.$folder_2_save_path);
